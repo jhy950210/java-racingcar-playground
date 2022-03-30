4f55d6d03ce50d7156f550046f090d64868c1677
jhy950210 <66720964+jhy950210@users.noreply.github.com>
2022-03-30 18:07:34 +0900

    refactor(car): 상수 변수 선언

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,10 +1,11 @@
 public class Car {
+    private static final int FORWARD_NUMBER = 4;
     private static final int START_POSITION = 0;
 
     private final String name;
     private int position;
 
-    public Car(String carName) {
+    public Car(final String carName) {
         if(!ValidationUtils.isValidCarNameLength(carName)){
             throw new IllegalArgumentException("자동차 이름 길이 오류.");
         }
@@ -14,7 +15,7 @@ public class Car {
     }
 
     public void play(int number) {
-        if(number >= 4){
+        if(number >= FORWARD_NUMBER){
             position += 1;
         }
     }
