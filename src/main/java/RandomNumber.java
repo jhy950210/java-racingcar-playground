55a1b22024f384e0bfbb3099e11bdf70bf7ca6d3
jhy950210 <66720964+jhy950210@users.noreply.github.com>
2022-03-31 15:57:46 +0900

    refactor(validation): 숫자 비교를 랜덤 숫자 객체가 다루는게 낫다고 판단

--- src/main/java/RandomNumber.java
+++ src/main/java/RandomNumber.java
@@ -1,13 +1,19 @@
 import java.util.Random;
 
 public class RandomNumber {
+    private final static int MAX_BOUND = 10;
     private final int number;
 
     public RandomNumber() {
-        this.number = (int)(Math.random() * 10);
+        Random random = new Random();
+        this.number = random.nextInt(MAX_BOUND);
     }
 
     public int getNumber() {
         return number;
+    }
+
+    public boolean greaterThan(int forwardNumber) {
+        return number >= forwardNumber;
     }
 }
